cf498dd87db24da6191095d70fe6b47e6349b919
AswinRam4433 <aswinarv55@gmail.com>
2023-12-19 11:29:28 +0530

    Horizontal and Vertical check

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	addCoin(&board, 3, 1)
 	addCoin(&board, 4, 2)
 	addCoin(&board, 3, 1)
+	addCoin(&board, 4, 2)
+	addCoin(&board, 3, 1)
+	addCoin(&board, 4, 2)
+	addCoin(&board, 3, 1)
+	// pass the arguments as references because the functions now take params by reference
 
 }
 
@@ -23,7 +28,7 @@ func displayBoard(board *[8][8]int) {
 	}
 }
 func addCoin(board *[8][8]int, col int, turn int) {
-
+	// pass the args by reference to cause the states to persist
 	// col specifies the column into which we have to drop the coin. Ranges from 0 to 7
 	// turn specifies which player is going to play. Alternates between 1 and 2
 
@@ -44,6 +49,10 @@ func addCoin(board *[8][8]int, col int, turn int) {
 	checkValidBoard(board)
 
 	displayBoard(board)
+	ans := checkSolved(board)
+	if ans != -1 {
+		fmt.Println("We have a winner  ", ans)
+	}
 }
 func checkValidBoard(board *[8][8]int) {
 	fmt.Println("Checking if board is valid ")
@@ -73,7 +82,38 @@ func initBoard(board *[8][8]int) {
 	displayBoard(board)
 }
 
-func checkSolved(board *[8][8]int) {
+func transpose(a *[8][8]int) [][]int {
+	newArr := make([][]int, len(*a))
+	for i := 0; i < len(*a); i++ {
+		for j := 0; j < len((*a)[0]); j++ {
+			newArr[j] = append(newArr[j], (*a)[i][j])
+		}
+	}
+	return newArr
+}
+
+func checkSolved(board *[8][8]int) int {
 	fmt.Println("Checking for winner: ")
+
+	// checking row wise
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 5; j++ {
+			if board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2] && board[i][j+2] == board[i][j+3] && board[i][j+1] != 0 {
+				return board[i][j]
+			}
+		}
+	}
+
+	// checking column wise
+	b_trans := transpose(board)
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 5; j++ {
+			if b_trans[i][j] == b_trans[i][j+1] && b_trans[i][j+1] == b_trans[i][j+2] && b_trans[i][j+2] == b_trans[i][j+3] && b_trans[i][j+2] != 0 {
+				return b_trans[i][j]
+			}
+		}
+	}
+
+	return -1
 
 }
